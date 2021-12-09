302a1d378db1ba0e44f9a7564b948898e37da014
Jessica Galarza <jessicaga941@gmail.com>
2021-12-08 23:36:48 -0500

    imported seachGoogleBooks and updated useEffect hook and created a method to search for books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -13,58 +13,39 @@ const SavedBooks = () => {
   // use this to determine if `useEffect()` hook needs to run again
   const userData = data?.me || [];
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+ 
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+        
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   // useMutation(()=>{
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    const handleDeleteBook = async (bookId) => {
+      
+      const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      if (!token) {
+        return false;
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
+try {
+       
+  await removeBook({
+    variables:{ bookId },
+  })
+
+  
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
+  
+  
 
+   
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
@@ -85,12 +66,26 @@ const SavedBooks = () => {
           {userData.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                {book.image ? (
+                  <Card.Img 
+                  src={book.image} 
+                  alt={`The cover for ${book.title}`} 
+                  variant='top'
+                   />
+                 ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
+                  <p classname='small'>
+                  Link:{""}
+                  <a href={book.link} target="_blank" rel="noreferrer">
+                  {book.title}
+                </a>
+              </p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button 
+                  className='btn-block btn-danger' 
+                  onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book!
                   </Button>
                 </Card.Body>
